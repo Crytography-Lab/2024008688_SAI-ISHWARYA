1e71679a2e5d60ef85c552e6d4ce2a6efa0ec567
ishwaryasai <sraparth@student.gitam.edu>
2026-09-18 12:50:21 +0530

    Polyalphabetic_Cipher.py

--- Polyalphabetic Cipher.py
+++ Polyalphabetic Cipher.py
@@ -1,19 +1,16 @@
-# Polyalphabetic Cipher
+// Polyalphabetic Cipher
 
 text = input("Enter message: ").upper()
 key = input("Enter key: ").upper()
 
-# Encryption
+// Encryption
 encrypted = ""
-
 for i in range(len(text)):
     encrypted += chr((ord(text[i]) - 65 + ord(key[i % len(key)]) - 65) % 26 + 65)
-
 print("Encrypted:", encrypted)
 
-# Decryption
+//Decryption
 decrypted = ""
-
 for i in range(len(encrypted)):
     decrypted += chr((ord(encrypted[i]) - 65 - (ord(key[i % len(key)]) - 65)) % 26 + 65)
 
