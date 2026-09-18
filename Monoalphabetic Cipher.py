4eac3d3d99824573a0baf432db5c5b7f0a0b2d3d
ishwaryasai <sraparth@student.gitam.edu>
2026-09-18 12:47:03 +0530

    Monoalphabetic_Cipher.py

--- Monoalphabetic Cipher.py
+++ Monoalphabetic Cipher.py
@@ -1,28 +1,23 @@
-# Monoalphabetic Cipher
+// Monoalphabetic Cipher
 
 text = input("Enter message: ")
-
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 key =     "QWERTYUIOPASDFGHJKLZXCVBNM"
 
-# Encryption
+// Encryption
 encrypted = ""
-
 for ch in text.upper():
     if ch in alphabet:
         encrypted += key[alphabet.index(ch)]
     else:
         encrypted += ch
-
 print("Encrypted:", encrypted)
 
-# Decryption
+// Decryption
 decrypted = ""
-
 for ch in encrypted:
     if ch in key:
         decrypted += alphabet[key.index(ch)]
     else:
         decrypted += ch
-
 print("Decrypted:", decrypted)
