ecc986c5a20bcb990ceb858543a4026f0a04ad7f
ishwaryasai <sraparth@student.gitam.edu>
2026-09-18 12:51:09 +0530

    Rail_Fence_Cipher.py

--- Rail Fence Cipher.py
+++ Rail Fence Cipher.py
@@ -1,11 +1,10 @@
-# Rail Fence Cipher
+//Rail Fence Cipher
 
 text = input("Enter text: ").replace(" ", "").upper()
 
-# Encryption
+//Encryption
 rail1 = ""
 rail2 = ""
-
 for i in range(len(text)):
     if i % 2 == 0:
         rail1 += text[i]
@@ -15,17 +14,14 @@ for i in range(len(text)):
 encrypted = rail1 + rail2
 print("Encrypted:", encrypted)
 
-# Decryption
+//Decryption
 mid = (len(encrypted) + 1) // 2
-
 rail1 = encrypted[:mid]
 rail2 = encrypted[mid:]
 
 decrypted = ""
-
 for i in range(len(rail1)):
     decrypted += rail1[i]
     if i < len(rail2):
         decrypted += rail2[i]
-
 print("Decrypted:", decrypted)
